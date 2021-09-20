e89533e16d9e8c501e648266467823142e0a3300
Artur Stefan <arthurstephan13@gmail.com>
2021-09-19 23:41:58 -0300

    feat: validating if it's a number

--- src/main/java/br/com/erudio/MathController.java
+++ src/main/java/br/com/erudio/MathController.java
@@ -25,8 +25,10 @@ public class MathController {
 
 	}
 
-	public boolean isNumeric(String number) {
-		return false;
+	public boolean isNumeric(String strNumber) {
+		if(strNumber == null) return false;
+		String number = strNumber.replaceAll(",", ".");
+		return number.matches("[-+]?[0-9]*\\\\.?[0-9]+");
 	}
 	
 }
