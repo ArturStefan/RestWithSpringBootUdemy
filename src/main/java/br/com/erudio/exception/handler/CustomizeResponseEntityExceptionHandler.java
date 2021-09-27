11c7ba137848fd1b4e172f668459ca7a219581bd
Artur Stefan <arthurstephan13@gmail.com>
2021-09-27 00:13:12 -0300

    fix: change bad request for not found

--- src/main/java/br/com/erudio/exception/handler/CustomizeResponseEntityExceptionHandler.java
+++ src/main/java/br/com/erudio/exception/handler/CustomizeResponseEntityExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.erudio.exception.ExceptionResponse;
-import br.com.erudio.exception.UnsupportedMathOperationsException;
+import br.com.erudio.exception.ResourceNotFoundException;
 
 @ControllerAdvice
 @RestController
@@ -28,14 +28,14 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
 		
 	}
 	
-	@ExceptionHandler(UnsupportedMathOperationsException.class)
+	@ExceptionHandler(ResourceNotFoundException.class)
 	public final ResponseEntity<ExceptionResponse> handleBadRequestExcpetions(Exception ex, WebRequest request){
 		ExceptionResponse exceptionResponse = 
 				new ExceptionResponse(
 						new Date(), 
 						ex.getMessage(), 
 						request.getDescription(false));
-		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
 		
 	}
 	
